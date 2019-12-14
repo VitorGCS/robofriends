3891a2566602a972ae91bf63d8875c8264d99aea
Vitor Silva <vitorgilcs@gmail.com>
2019-12-14 15:19:56 +0000

    removing static users/robots, get users/robots from JSONPlaceholder API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots';
+
 
 class App extends Component {
     constructor(){
         super();
         this.state = {
-            robots:robots,
+            robots:[],
             searchfield:''
         }
     }
     
+    //add Lifecycle method componentDidMount() to get dynamically users
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}));  
+    }
+
     onSearchChange = (event) => {
         //React provides a method called setState() to change the state
         this.setState({searchfield:event.target.value})
@@ -22,14 +29,17 @@ class App extends Component {
         const filterRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-        //console.log(filterRobots);
-        return(
-            <div className='tc'>
-                <h1>RoboFriends</h1>
-                < SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filterRobots}/>
-            </div>
-        );
+        if(this.state.robots.length === 0){
+            return <h1>Loading</h1>
+        }else{
+            return(
+                <div className='tc'>
+                    <h1>RoboFriends</h1>
+                    < SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filterRobots}/>
+                </div>
+            );
+        }
     } 
 }
 
